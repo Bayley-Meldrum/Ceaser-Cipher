2ba312a191caf8a11bf4aa8e29c2bc54d584ca36
Bayley-Meldrum <159397191+Bayley-Meldrum@users.noreply.github.com>
2024-06-24 09:27:31 +1200

    Update Main.py
    Added function for obtaining user's message input, and locked it down to only accept letters.
    Added function for obtaining user's shift input, and locked it down to only accept positive numbers.
    Added function for shifting user's message across the alphabet, and added function to skip shifting things that aren't letters

---
+++ Main.py
@@ -0,0 +1,36 @@
+while True:
+    message = input("Enter a message: ")
+    if all(char.isalpha() for char in message):
+        break
+    else:
+        print("Error. Please enter a valid message that only contains letters.")
+
+while True:
+    shift = int(input("Enter your secret number: "))
+    if shift >0:
+        break
+    else:
+        print("Error. PLease enter a valid number that is greater than 0.")
+
+def decrypt(message, shift):
+    decrypted_message = ""
+
+    # Traverse text
+    for char in message:
+        # Skip shifting spaces
+        if char.isspace():
+            decrypted_message += char
+        # Decrypt uppercase characters
+        elif char.isupper():
+            decrypted_message += chr((ord(char) - shift - 65) % 26 + 65)
+        # Decrypt lowercase characters
+        else:
+            decrypted_message += chr((ord(char) - shift - 97) % 26 + 97)
+
+    return decrypted_message
+
+# Outputs the text that the user put in, along with a decrypted version and the secret number
+print("Input Text:", message)
+print("Secret Number:", shift)
+decrypted_text = decrypt(message, shift)
+print("Output Text:", decrypted_text)
