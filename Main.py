d5423cd69547bee98f1472d48a045ee2061ed589
Bayley-Meldrum <159397191+Bayley-Meldrum@users.noreply.github.com>
2024-06-24 09:12:36 +1200

    Update Main.py
    modified Decrypt function to be more robust and flexible

--- Main.py
+++ Main.py
@@ -1,19 +1,26 @@
 
 message = input("Enter a message: ")
-
 shift = int(input("Enter your secret number: "))
 
-def shift_message(message, shift):
-    shifted_message = ""
-    for char in message:
-        if char.isalpha():
-            if char.isupper():
-                shifted_char = chr((ord(char) - 65 - shift) % 26 + 65)
-            else:
-                shifted_char = chr((ord(char) - 97 - shift) % 26 + 97)
-        else:
-            shifted_char = char
-        shifted_message += shifted_char
-    return shifted_message
+def decrypt(message, shift):
+    decrypted_message = ""
 
-print(shift_message(message, shift))
+    # Traverse text
+    for char in message:
+        # Skip shifting spaces
+        if char.isspace():
+            decrypted_message += char
+        # Decrypt uppercase characters
+        elif char.isupper():
+            decrypted_message += chr((ord(char) - shift - 65) % 26 + 65)
+        # Decrypt lowercase characters
+        else:
+            decrypted_message += chr((ord(char) - shift - 97) % 26 + 97)
+
+    return decrypted_message
+
+# Outputs the text that the user put in, along with a decrypted version and the secret number
+print("Input Text:", message)
+print("Secret Number:", shift)
+decrypted_text = decrypt(message, shift)
+print("Output Text:", decrypted_text)
