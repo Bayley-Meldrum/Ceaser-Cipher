95901f198dbd428b0e43388ccc8ef572c749f2cc
Bayley-Meldrum <159397191+Bayley-Meldrum@users.noreply.github.com>
2024-06-25 12:24:28 +1200

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,23 +11,21 @@ while True:
         break
     else:
         print("Error. PLease enter a valid number that is greater than 0.")
-
+#function to decrypt the user's message
 def decrypt(message, shift):
     decrypted_message = ""
 
     # Traverse text
     for char in message:
-        # Skip shifting spaces
-        if char.isspace():
-            decrypted_message += char
-        # Decrypt uppercase characters
-        elif char.isupper():
+        #if characters are upper case, decrypt using this line of code
+        if char.isupper():
             decrypted_message += chr((ord(char) - shift - 65) % 26 + 65)
-        # Decrypt lowercase characters
+        # If Character's arent upper case, user this line of code to decrpy the message
         else:
             decrypted_message += chr((ord(char) - shift - 97) % 26 + 97)
 
     return decrypted_message
+
 
 # Outputs the text that the user put in, along with a decrypted version and the secret number
 print("Input Text:", message)
